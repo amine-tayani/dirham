e7da0f1f515267136bca17ba9f589555a046f871
amine-tayani <amine.tayanipro@gmail.com>
2025-06-05 13:24:57 +0100

    feat: add sooner toast notification

--- src/routes/(auth)/login.tsx
+++ src/routes/(auth)/login.tsx
@@ -1,9 +1,13 @@
 import { Button } from "@/components/ui/button";
+import { Form, FormControl, FormField, FormItem, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import authClient from "@/lib/auth-client";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { Link, createFileRoute, useNavigate } from "@tanstack/react-router";
 import { LoaderCircle } from "lucide-react";
 import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 
 export const Route = createFileRoute("/(auth)/login")({
 	component: LoginForm
@@ -16,63 +20,91 @@ function LoginForm() {
 	const [isLoading, setIsLoading] = useState(false);
 	const [errorMessage, setErrorMessage] = useState("");
 
-	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-		e.preventDefault();
-		if (isLoading) return;
+	const loginSchema = z.object({
+		email: z.string().email({ message: "Invalid email address" }),
+		password: z.string().min(8, { message: "Password must be at least 8 characters long" })
+	});
 
-		const formData = new FormData(e.currentTarget);
-		const email = formData.get("email") as string;
-		const password = formData.get("password") as string;
-		if (!email || !password) return;
+	const form = useForm<z.infer<typeof loginSchema>>({
+		resolver: zodResolver(loginSchema),
+		defaultValues: {
+			email: "",
+			password: ""
+		}
+	});
 
+	const onLoginSubmit = async (values: z.infer<typeof loginSchema>) => {
 		setIsLoading(true);
 		setErrorMessage("");
-
-		authClient.signIn.email(
-			{
-				email,
-				password,
-				callbackURL: redirectUrl
-			},
-			{
-				onError: (ctx) => {
-					setErrorMessage(ctx.error.message);
-					setIsLoading(false);
+		try {
+			await authClient.signIn.email(
+				{
+					email: values.email,
+					password: values.password,
+					callbackURL: redirectUrl
 				},
-				onSuccess: async () => {
-					await queryClient.invalidateQueries({ queryKey: ["user"] });
-					navigate({ to: redirectUrl });
+				{
+					onError: (ctx) => {
+						setIsLoading(false);
+					},
+					onSuccess: async () => {
+						await queryClient.invalidateQueries({ queryKey: ["user"] });
+						navigate({ to: redirectUrl });
+					}
 				}
-			}
-		);
+			);
+			navigate({ to: redirectUrl || "/dashboard" });
+		} catch (err: any) {
+			setErrorMessage(err.message || "Login failed");
+		} finally {
+			setIsLoading(false);
+		}
 	};
 
 	return (
 		<div className="flex flex-col gap-6 font-geist">
-			<form onSubmit={handleSubmit}>
-				<div className="flex flex-col gap-6">
-					<h1 className="text-center text-2xl font-bold ">Log in to Dirhamly</h1>
-					<div className="flex flex-col gap-5 mt-4">
-						<div className="grid gap-2">
-							<Input
-								className="h-12 border border-neutral-700 shadow-none dark:border-none"
-								name="email"
-								type="email"
-								placeholder="example@gmail.com"
-								readOnly={isLoading}
-								required
-							/>
-						</div>
-						<div className="grid gap-2">
-							<Input
-								className="h-12 border border-neutral-700 shadow-none dark:border-none"
-								name="password"
-								type="password"
-								placeholder="Your password"
-								readOnly={isLoading}
-								required
-							/>
-						</div>
+			<Form {...form}>
+				<form onSubmit={form.handleSubmit(onLoginSubmit)} className="flex flex-col gap-6">
+					<div className="flex flex-col gap-6">
+						<h1 className="text-center text-2xl font-bold ">Log in to Dirhamly</h1>
+						<FormField
+							control={form.control}
+							name="email"
+							render={({ field }) => (
+								<FormItem>
+									<FormControl>
+										<Input
+											{...field}
+											className="h-12 border border-neutral-700 shadow-none dark:border-none"
+											placeholder="example@gmail.com"
+											readOnly={isLoading}
+											required
+										/>
+									</FormControl>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+						<FormField
+							control={form.control}
+							name="password"
+							render={({ field }) => (
+								<FormItem>
+									<FormControl>
+										<Input
+											{...field}
+											type="password"
+											className="h-12 border border-neutral-700 shadow-none dark:border-none"
+											placeholder="Your password"
+											readOnly={isLoading}
+											required
+										/>
+									</FormControl>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+
 						<Button
 							type="submit"
 							className="group mt-2 h-12 bg-blue-700 hover:bg-blue-800 text-white font-sans font-semibold w-full"
@@ -91,9 +123,7 @@ function LoginForm() {
 							</div>
 						</Button>
 					</div>
-					{errorMessage && (
-						<span className="text-destructive text-center text-sm">{errorMessage}</span>
-					)}
+					{errorMessage && <span className="text-red-500 text-center text-sm">{errorMessage}</span>}
 					<div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
 						<span className="bg-background text-muted-foreground relative z-10 px-2">Or</span>
 					</div>
@@ -126,12 +156,11 @@ function LoginForm() {
 							Login with Google
 						</Button>
 					</div>
-				</div>
-			</form>
-
+				</form>
+			</Form>
 			<div className="text-center text-sm">
 				Don&apos;t have an account?{" "}
-				<Link to="/signup" className="underline underline-offset-4">
+				<Link to="/signup" className="text-primary hover:text-primary/80">
 					Sign up
 				</Link>
 			</div>
