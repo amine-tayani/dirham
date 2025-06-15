8c4e15a846b4e10c53b565b210c1118f415ea044
amine-tayani <amine.tayanipro@gmail.com>
2025-06-15 20:01:55 +0100

    change favicon and logo

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -4,16 +4,16 @@ import type { QueryClient } from "@tanstack/react-query";
 import { Link } from "@tanstack/react-router";
 import type { User } from "better-auth";
 import {
-  ArrowLeftRightIcon,
-  BellIcon,
-  CalendarDaysIcon,
-  ClipboardIcon,
-  EclipseIcon,
-  HomeIcon,
-  LifeBuoy,
-  MoonIcon,
-  SettingsIcon,
-  WalletCardsIcon
+	ArrowLeftRightIcon,
+	BellIcon,
+	CalendarDaysIcon,
+	ClipboardIcon,
+	EclipseIcon,
+	HomeIcon,
+	LifeBuoy,
+	MoonIcon,
+	SettingsIcon,
+	WalletCardsIcon
 } from "lucide-react";
 import type React from "react";
 import Logo from "../logo";
@@ -22,83 +22,86 @@ import { MainNav } from "./main-nav";
 import { SupportNav } from "./support-nav";
 
 const data = {
-  mainNavigation: [
-    {
-      title: "Dashboard",
-      url: "/dashboard",
-      icon: HomeIcon
-    },
-    {
-      title: "Accounts",
-      url: "#",
-      icon: WalletCardsIcon
-    },
-    {
-      title: "Transactions",
-      url: "#",
-      icon: ArrowLeftRightIcon
-    },
-    {
-      title: "Spending Plan",
-      url: "#",
-      icon: EclipseIcon
-    },
-    {
-      title: "Upcomings",
-      url: "#",
-      icon: CalendarDaysIcon
-    },
-    {
-      title: "Reports",
-      url: "#",
-      icon: ClipboardIcon
-    }
-  ],
-  supportNav: [
-    {
-      title: "Notifications",
-      url: "#",
-      icon: BellIcon
-    },
-    {
-      title: "Help Center",
-      url: "#",
-      icon: LifeBuoy
-    },
-    {
-      title: "Settings",
-      url: "#",
-      icon: SettingsIcon
-    }
-  ]
+	mainNavigation: [
+		{
+			title: "Dashboard",
+			url: "/dashboard",
+			icon: HomeIcon
+		},
+		{
+			title: "Accounts",
+			url: "#",
+			icon: WalletCardsIcon
+		},
+		{
+			title: "Transactions",
+			url: "#",
+			icon: ArrowLeftRightIcon
+		},
+		{
+			title: "Spending Plan",
+			url: "#",
+			icon: EclipseIcon
+		},
+		{
+			title: "Upcomings",
+			url: "#",
+			icon: CalendarDaysIcon
+		},
+		{
+			title: "Reports",
+			url: "#",
+			icon: ClipboardIcon
+		}
+	],
+	supportNav: [
+		{
+			title: "Notifications",
+			url: "#",
+			icon: BellIcon
+		},
+		{
+			title: "Help Center",
+			url: "#",
+			icon: LifeBuoy
+		},
+		{
+			title: "Settings",
+			url: "#",
+			icon: SettingsIcon
+		}
+	]
 };
 
 interface Props extends React.ComponentProps<typeof Sidebar> {
-  user: User;
-  queryClient: QueryClient;
+	user: User;
+	queryClient: QueryClient;
 }
 
 export function DashboardSidebar({ user, queryClient, ...props }: Props) {
-  return (
-    <Sidebar collapsible="icon" {...props}>
-      <SidebarHeader>
-        <SidebarMenuItem>
-          <div className="flex items-center justify-between mx-2 my-2">
-            <Link to="/">
-              <Logo className="size-8" />
-            </Link>
-            <div className="group-data-[collapsible=icon]:hidden">
-              <Button variant="ghost" onClick={toggleTheme}>
-                <MoonIcon className="size-5" />
-              </Button>
-            </div>
-          </div>
-        </SidebarMenuItem>
-      </SidebarHeader>
-      <SidebarContent>
-        <MainNav items={data.mainNavigation} />
-        <SupportNav items={data.supportNav} />
-      </SidebarContent>
-    </Sidebar>
-  );
+	return (
+		<Sidebar collapsible="icon" {...props}>
+			<SidebarHeader>
+				<SidebarMenuItem>
+					<div className="flex items-center justify-between mx-2 my-2">
+						<Link to="/" className="flex items-center">
+							<Logo className="group-data-[collapsible=icon]:size-10" />
+							<span className="mr-2 font-geist font-semibold text-card-foreground dark:text-primary-foreground group-data-[collapsible=icon]:hidden">
+								Dirhamly
+							</span>
+						</Link>
+						<div className="group-data-[collapsible=icon]:hidden">
+							<Button variant="ghost" onClick={toggleTheme}>
+								<MoonIcon className="size-5" />
+							</Button>
+						</div>
+					</div>
+				</SidebarMenuItem>
+			</SidebarHeader>
+			<SidebarContent>
+				<MainNav items={data.mainNavigation} />
+				<SupportNav items={data.supportNav} />
+			</SidebarContent>
+		</Sidebar>
+	);
 }
--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -460,7 +460,7 @@ const sidebarMenuButtonVariants = cva(
 				default: "hover:bg-sidebar-accent hover:text-sidebar-accent-foreground",
 				ghost: "hover:bg-transparent hover:text-sidebar-foreground",
 				outline:
-					"bg-background shadow-[0_0_0_1px_hsl(var(--sidebar-border))] hover:bg-sidebar-accent hover:text-sidebar-accent-foreground hover:shadow-[0_0_0_1px_hsl(var(--sidebar-accent))]"
+					"bg-background shadow-[0_0_0_1px_hsl(var(--sidebar-border))] hover:bg-sidebar-accent hover:text-sidebar-accent-foreground  hover:shadow-[0_0_0_1px_hsl(var(--sidebar-accent))]"
 			},
 			size: {
 				default: "h-8 text-sm",
@@ -541,7 +541,7 @@ function SidebarMenuAction({
 			data-slot="sidebar-menu-action"
 			data-sidebar="menu-action"
 			className={cn(
-				"text-sidebar-foreground ring-sidebar-ring hover:bg-sidebar-accent hover:text-sidebar-accent-foreground peer-hover/menu-button:text-sidebar-accent-foreground absolute top-1.5 right-1 flex aspect-square w-5 items-center justify-center rounded-md p-0 outline-hidden transition-transform focus-visible:ring-2 [&>svg]:size-4 [&>svg]:shrink-0",
+				"text-sidebar-foreground ring-sidebar-ring hover:bg-sidebar-accent hover:text-sidebar-accent-foreground peer-hover/menu-button:text-sidebar-accent-foreground absolute top-1.5 right-1 flex aspect-square w-5 items-center justify-center rounded-lg p-0 outline-hidden transition-transform focus-visible:ring-2 [&>svg]:size-4 [&>svg]:shrink-0",
 				// Increases the hit area of the button on mobile.
 				"after:absolute after:-inset-2 md:after:hidden",
 				"peer-data-[size=sm]/menu-button:top-1",
