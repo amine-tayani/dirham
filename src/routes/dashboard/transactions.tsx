60736d52f6dd26ca99de024595e3ab4a5d83875b
amine-tayani <amine.tayanipro@gmail.com>
2025-06-23 21:40:30 +0100

    Create a transactions page and populate it

--- src/routes/dashboard/transactions.tsx
+++ src/routes/dashboard/transactions.tsx
@@ -1,32 +1,19 @@
+import { InsightCard } from "@/components/dashboard/insight-card";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/dashboard/transactions")({
 	component: RouteComponent
 });
 
-export const statsData = [
-	{
-		title: "Budget",
-		value: "$20,250",
-		changePercent: "+4.6%"
-	},
-
-	{
-		title: "Expenditure",
-		value: "$14,250",
-		changePercent: "-4%"
-	},
-	{
-		title: "Income",
-		value: "$17,650",
-		changePercent: "+4.3%"
-	}
-];
-
 function RouteComponent() {
 	return (
 		<div className="flex flex-col gap-4 p-8">
 			<h1 className="text-xl font-semibold">Overview</h1>
+			<div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+				<InsightCard title="Budget" value="$20,250" changePercent="+25.00%" />
+				<InsightCard title="Expends" value="$14,350" changePercent="+8.75%" />
+				<InsightCard title="Income" value="$17,550" changePercent="-24.50%" />
+			</div>
 		</div>
 	);
 }
