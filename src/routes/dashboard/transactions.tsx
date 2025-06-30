90d3d831e78bb309f3acfdd60f1feb8f045cc93c
amine-tayani <amine.tayanipro@gmail.com>
2025-06-30 17:37:31 +0100

    feat: add transactions table

--- src/routes/dashboard/transactions.tsx
+++ src/routes/dashboard/transactions.tsx
@@ -1,4 +1,4 @@
-import { InsightCard } from "@/components/dashboard/insight-card";
+import { TransactionsTable } from "@/components/dashboard/transactions-table";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/dashboard/transactions")({
@@ -8,12 +8,8 @@ export const Route = createFileRoute("/dashboard/transactions")({
 function RouteComponent() {
 	return (
 		<div className="flex flex-col gap-4">
-			<h1 className="text-xl font-semibold">Overview</h1>
-			<div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-				<InsightCard title="Budget" value="$20,250" changePercent="+25.00%" />
-				<InsightCard title="Expends" value="$14,350" changePercent="+8.75%" />
-				<InsightCard title="Income" value="$17,550" changePercent="-24.50%" />
-			</div>
+			<h1 className="text-xl font-semibold">Transactions</h1>
+			<TransactionsTable />
 		</div>
 	);
 }
