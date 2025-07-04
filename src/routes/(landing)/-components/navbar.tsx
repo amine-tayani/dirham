8d25e5d9cbef81675c083f8c25fb50eec0fdabc7
amine-tayani <amine.tayanipro@gmail.com>
2025-07-04 18:38:10 +0100

    fix: mobile navbar menu closing icon

--- src/routes/(landing)/-components/navbar.tsx
+++ src/routes/(landing)/-components/navbar.tsx
@@ -36,16 +36,17 @@ export default function Navbar() {
 					)}
 				>
 					<div className="relative flex flex-wrap items-center justify-between gap-6 py-3 lg:gap-0 lg:py-4">
-						<div className="flex w-full justify-between lg:w-auto">
-							<Link to="/" aria-label="home" className="flex items-center">
+						<div className="flex w-full justify-between items-center lg:w-auto">
+							<Link to="/" aria-label="Logo" className="flex items-center">
 								<Logo />
 								<span className="text-lg font-bold font-mont tracking-tighter ml-1 ">Dirhamly</span>
 							</Link>
 
 							<Button
+								variant="ghost"
 								onClick={() => setMenuState(!menuState)}
 								aria-label={menuState === true ? "Close Menu" : "Open Menu"}
-								className="relative z-20 -m-2.5 -mr-4 block cursor-pointer p-2.5 lg:hidden"
+								className="relative z-20 mr-2 rounded-full cursor-pointer size-10 flex items-center justify-center lg:hidden"
 							>
 								<MenuIcon className="in-data-[state=active]:rotate-180 group-data-[state=active]:scale-0 group-data-[state=active]:opacity-0 m-auto size-6 duration-200" />
 								<XIcon className="group-data-[state=active]:rotate-0 group-data-[state=active]:scale-100 group-data-[state=active]:opacity-100 absolute inset-0 m-auto size-6 -rotate-180 scale-0 opacity-0 duration-200" />
@@ -67,14 +68,14 @@ export default function Navbar() {
 							</ul>
 						</div>
 
-						<div className="bg-background group-data-[state=active]:block lg:group-data-[state=active]:flex mb-6 hidden w-full flex-wrap items-center justify-end space-y-8 rounded-3xl border p-6 shadow-2xl shadow-zinc-300/20 md:flex-nowrap lg:m-0 lg:flex lg:w-fit lg:gap-6 lg:space-y-0 lg:border-transparent lg:bg-transparent lg:p-0 lg:shadow-none dark:shadow-none dark:lg:bg-transparent">
+						<div className="bg-background group-data-[state=active]:block lg:group-data-[state=active]:flex mb-6 hidden w-full flex-wrap items-center text-center space-y-5 rounded-xl border p-10 md:flex-nowrap lg:m-0 lg:flex lg:w-fit lg:gap-6 lg:space-y-0 lg:border-transparent lg:bg-transparent lg:p-0 lg:shadow-none dark:shadow-none dark:lg:bg-transparent">
 							<div className="lg:hidden">
-								<ul className="space-y-6 text-base">
+								<ul className="space-y-8 text-2xl font-medium">
 									{menuItems.map((item, index) => (
 										<li key={item.name}>
 											<Link
 												to={item.href}
-												className="text-muted-foreground hover:text-accent-foreground block duration-150"
+												className="text-muted-foreground/70 hover:text-accent-foreground block duration-150"
 											>
 												<span>{item.name}</span>
 											</Link>
@@ -85,7 +86,7 @@ export default function Navbar() {
 							{isPending ? (
 								<Loader2Icon className="animate-spin" />
 							) : !session ? (
-								<div className="flex w-full flex-col space-y-3 sm:flex-row sm:gap-3 sm:space-y-0 md:w-fit">
+								<div className="hidden md:flex w-full flex-col space-y-3 md:flex-row md:gap-3 md:space-y-0 md:w-fit">
 									<Button asChild variant="outline" className={cn(isScrolled && "lg:hidden")}>
 										<Link to="/login">
 											<span className="dark:text-white font-semibold text-nowrap font-mont">
@@ -109,7 +110,7 @@ export default function Navbar() {
 									</Button>
 								</div>
 							) : (
-								<Button asChild>
+								<Button asChild className="hidden md:block">
 									<Link to="/dashboard">
 										<span className="text-white font-semibold text-nowrap font-mont">
 											Dashboard
