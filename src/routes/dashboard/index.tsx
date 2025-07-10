9462080464ff781caf199b6cbe43cf453e6b3ca3
amine-tayani <amine.tayanipro@gmail.com>
2025-07-10 11:50:15 +0100

    fix: color match for sidebar items

--- src/routes/dashboard/index.tsx
+++ src/routes/dashboard/index.tsx
@@ -44,16 +44,14 @@ function DashboardIndex() {
 			<header className="flex h-20 shrink-0 items-center gap-2 border-b pt-2 mb-4">
 				<div className="flex flex-1 items-center gap-2">
 					<div className="flex items-center justify-between gap-4 pl-7">
-						<h1 className="text-2xl font-medium font-mono tracking-tighter">
-							{`${greetUser()} ${user.name}`}
-						</h1>
+						<h1 className="text-2xl font-medium tracking-tight">{`${greetUser()} ${user.name}`}</h1>
 					</div>
 				</div>
 				<div className="flex ml-auto pr-7">
 					<UserNav user={user} queryClient={queryClient} />
 				</div>
 			</header>
-			<div className="flex flex-1 flex-col gap-4 lg:gap-6 mt-4">
+			<div className="flex flex-1 flex-col gap-4 lg:gap-6 mt-4 mx-7 mb-4">
 				<div className="grid grid-cols-2 min-[1200px]:grid-cols-4 gap-4">
 					{data.map((item) => (
 						<InsightCard
