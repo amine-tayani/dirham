7bb2fa5294cc2c75e744993280738dc099feba93
amine-tayani <amine.tayanipro@gmail.com>
2025-08-10 00:32:21 +0100

    chore: move Navbar to landing route

--- src/routes/(landing)/-components/hero.tsx
+++ src/routes/(landing)/-components/hero.tsx
@@ -2,7 +2,6 @@ import { AnimatedGroup } from "@/components/ui/animated-group";
 import { Button } from "@/components/ui/button";
 import { Link } from "@tanstack/react-router";
 import { ArrowRight, ChevronRight } from "lucide-react";
-import Navbar from "./navbar";
 
 const transitionVariants = {
 	item: {
@@ -26,152 +25,149 @@ const transitionVariants = {
 
 export function HeroSection() {
 	return (
-		<>
-			<Navbar />
-			<main className="overflow-hidden">
-				<div
-					aria-hidden
-					className="z-[2] absolute inset-0 pointer-events-none isolate opacity-50 contain-strict hidden lg:block"
-				>
-					<div className="w-[35rem] h-[80rem] -translate-y-[350px] absolute left-0 top-0 -rotate-45 rounded-full bg-[radial-gradient(68.54%_68.72%_at_55.02%_31.46%,hsla(0,0%,85%,.08)_0,hsla(0,0%,55%,.02)_50%,hsla(0,0%,45%,0)_80%)]" />
-					<div className="h-[80rem] absolute left-0 top-0 w-56 -rotate-45 rounded-full bg-[radial-gradient(50%_50%_at_50%_50%,hsla(0,0%,85%,.06)_0,hsla(0,0%,45%,.02)_80%,transparent_100%)] [translate:5%_-50%]" />
-					<div className="h-[80rem] -translate-y-[350px] absolute left-0 top-0 w-56 -rotate-45 bg-[radial-gradient(50%_50%_at_50%_50%,hsla(0,0%,85%,.04)_0,hsla(0,0%,45%,.02)_80%,transparent_100%)]" />
-				</div>
-				<section>
-					<div className="relative pt-24 md:pt-36">
-						<div
-							aria-hidden
-							className="absolute inset-0 -z-10 size-full [background:radial-gradient(125%_125%_at_50%_100%,transparent_0%,var(--background)_75%)]"
-						/>
-						<div className="mx-auto max-w-7xl px-6">
-							<div className="text-center sm:mx-auto lg:mr-auto lg:mt-0">
-								<AnimatedGroup variants={transitionVariants}>
-									<Link
-										to="/dashboard"
-										className="hover:bg-neutral-200 dark:hover:border-t-border bg-muted group mx-auto flex w-fit items-center gap-4 rounded-full border p-1 pl-4 transition-all duration-300 dark:border-t-white/5 dark:shadow-zinc-950"
-									>
-										<span className="text-foreground text-sm">
-											Introducing The Next Generation of Personal Budgeting Apps
-										</span>
-										<span className="dark:border-background block h-4 w-0.5 border-l bg-white dark:bg-zinc-700" />
-
-										<div className="bg-background group-hover:bg-muted size-6 overflow-hidden rounded-full duration-500">
-											<div className="flex w-12 -translate-x-1/2 duration-500 ease-in-out group-hover:translate-x-0">
-												<span className="flex size-6">
-													<ArrowRight className="m-auto size-3" />
-												</span>
-												<span className="flex size-6">
-													<ArrowRight className="m-auto size-3" />
-												</span>
-											</div>
-										</div>
-									</Link>
-
-									<h1 className="mt-8 max-w-4xl font-geist font-semibold mx-auto text-balance text-6xl md:text-7xl lg:mt-12 xl:text-[5.25rem]">
-										Dirhamly Your Personal Budgeting App
-									</h1>
-									<p className="mx-auto mt-8 max-w-2xl text-balance text-lg font-geist">
-										Offering everything from smart expense tracking to savings goal planning, all
-										wrapped in an elegant interface with real-time guidance from AI
-									</p>
-								</AnimatedGroup>
-
-								<AnimatedGroup
-									variants={{
-										container: {
-											visible: {
-												transition: {
-													staggerChildren: 0.05,
-													delayChildren: 0.75
-												}
-											}
-										},
-										...transitionVariants
-									}}
-									className="mt-12 flex flex-col items-center justify-center gap-2 md:flex-row"
+		<main className="overflow-hidden">
+			<div
+				aria-hidden
+				className="z-[2] absolute inset-0 pointer-events-none isolate opacity-50 contain-strict hidden lg:block"
+			>
+				<div className="w-[35rem] h-[80rem] -translate-y-[350px] absolute left-0 top-0 -rotate-45 rounded-full bg-[radial-gradient(68.54%_68.72%_at_55.02%_31.46%,hsla(0,0%,85%,.08)_0,hsla(0,0%,55%,.02)_50%,hsla(0,0%,45%,0)_80%)]" />
+				<div className="h-[80rem] absolute left-0 top-0 w-56 -rotate-45 rounded-full bg-[radial-gradient(50%_50%_at_50%_50%,hsla(0,0%,85%,.06)_0,hsla(0,0%,45%,.02)_80%,transparent_100%)] [translate:5%_-50%]" />
+				<div className="h-[80rem] -translate-y-[350px] absolute left-0 top-0 w-56 -rotate-45 bg-[radial-gradient(50%_50%_at_50%_50%,hsla(0,0%,85%,.04)_0,hsla(0,0%,45%,.02)_80%,transparent_100%)]" />
+			</div>
+			<section>
+				<div className="relative pt-24 md:pt-36">
+					<div
+						aria-hidden
+						className="absolute inset-0 -z-10 size-full [background:radial-gradient(125%_125%_at_50%_100%,transparent_0%,var(--background)_75%)]"
+					/>
+					<div className="mx-auto max-w-7xl px-6">
+						<div className="text-center sm:mx-auto lg:mr-auto lg:mt-0">
+							<AnimatedGroup variants={transitionVariants}>
+								<Link
+									to="/dashboard"
+									className="hover:bg-neutral-200 dark:hover:border-t-border bg-muted group mx-auto flex w-fit items-center gap-4 rounded-full border p-1 pl-4 transition-all duration-300 dark:border-t-white/5 dark:shadow-zinc-950"
 								>
-									<Button asChild size="lg" className="text-white rounded-xl px-5">
-										<Link to="/signup">
-											<span className="text-nowrap font-geist text-sm">Start Building</span>
-										</Link>
-									</Button>
-									<Button
-										key={2}
-										asChild
-										size="lg"
-										variant="ghost"
-										className="h-10.5 rounded-xl px-5"
-									>
-										<Link to="/signup">
-											<span className="text-nowrap">Request a demo</span>
-										</Link>
-									</Button>
-								</AnimatedGroup>
-							</div>
-						</div>
+									<span className="text-foreground text-sm">
+										Introducing The Next Generation of Personal Budgeting Apps
+									</span>
+									<span className="dark:border-background block h-4 w-0.5 border-l bg-white dark:bg-zinc-700" />
 
-						<AnimatedGroup
-							variants={{
-								container: {
-									visible: {
-										transition: {
-											staggerChildren: 0.05,
-											delayChildren: 0.75
+									<div className="bg-background group-hover:bg-muted size-6 overflow-hidden rounded-full duration-500">
+										<div className="flex w-12 -translate-x-1/2 duration-500 ease-in-out group-hover:translate-x-0">
+											<span className="flex size-6">
+												<ArrowRight className="m-auto size-3" />
+											</span>
+											<span className="flex size-6">
+												<ArrowRight className="m-auto size-3" />
+											</span>
+										</div>
+									</div>
+								</Link>
+
+								<h1 className="mt-8 max-w-4xl font-geist font-semibold mx-auto text-balance text-6xl md:text-7xl lg:mt-12 xl:text-[5.25rem]">
+									Dirhamly Your Personal Budgeting App
+								</h1>
+								<p className="mx-auto mt-8 max-w-2xl text-balance text-lg font-geist">
+									Offering everything from smart expense tracking to savings goal planning, all
+									wrapped in an elegant interface with real-time guidance from AI
+								</p>
+							</AnimatedGroup>
+
+							<AnimatedGroup
+								variants={{
+									container: {
+										visible: {
+											transition: {
+												staggerChildren: 0.05,
+												delayChildren: 0.75
+											}
 										}
-									}
-								},
-								...transitionVariants
-							}}
-						>
-							<div className="relative -mr-56 mt-8 overflow-hidden px-2 sm:mr-0 sm:mt-12 md:mt-20">
-								<div
-									aria-hidden
-									className="bg-gradient-to-b to-background absolute inset-0 z-10 from-transparent from-35%"
-								/>
-								<div className="inset-shadow-2xs ring-background dark:inset-shadow-white/20 bg-background relative mx-auto max-w-6xl overflow-hidden rounded-2xl border p-4 shadow-lg shadow-zinc-950/15 ring-1">
-									<img
-										className="bg-background aspect-16/8 relative hidden rounded-2xl dark:block"
-										src="/screenshot.png"
-										alt="dark app screen"
-									/>
-									<img
-										className="z-2 aspect-16/8 relative rounded-2xl dark:hidden object-contain"
-										src="/screenshot.png"
-										alt="light app screen"
-									/>
-								</div>
-							</div>
-						</AnimatedGroup>
-					</div>
-				</section>
-				{/* we can move this section to a separate file */}
-				<section className="bg-background pb-16 pt-16 md:pb-32">
-					<div className="group relative m-auto max-w-5xl px-6">
-						<div className="absolute inset-0 z-10 flex scale-95 items-center justify-center opacity-0 duration-500 group-hover:scale-100 group-hover:opacity-100">
-							<Link to="/" className="block text-sm duration-150 hover:opacity-75">
-								<span> Our Customers</span>
-								<ChevronRight className="ml-1 inline-block size-3" />
-							</Link>
+									},
+									...transitionVariants
+								}}
+								className="mt-12 flex flex-col items-center justify-center gap-2 md:flex-row"
+							>
+								<Button asChild size="lg" className="text-white rounded-xl px-5">
+									<Link to="/signup">
+										<span className="text-nowrap font-geist text-sm">Start Building</span>
+									</Link>
+								</Button>
+								<Button
+									key={2}
+									asChild
+									size="lg"
+									variant="ghost"
+									className="h-10.5 rounded-xl px-5"
+								>
+									<Link to="/signup">
+										<span className="text-nowrap">Request a demo</span>
+									</Link>
+								</Button>
+							</AnimatedGroup>
 						</div>
-						<div className="group-hover:blur-xs mx-auto mt-12 grid max-w-2xl grid-cols-4 gap-x-12 gap-y-8 transition-all duration-500 group-hover:opacity-50 sm:gap-x-16 sm:gap-y-14">
-							{Array(8)
-								.fill(0)
-								.map((_, index) => (
-									<div className="flex" key={index}>
-										logo {index + 1}
-										{/* <img
+					</div>
+
+					<AnimatedGroup
+						variants={{
+							container: {
+								visible: {
+									transition: {
+										staggerChildren: 0.05,
+										delayChildren: 0.75
+									}
+								}
+							},
+							...transitionVariants
+						}}
+					>
+						<div className="relative -mr-56 mt-8 overflow-hidden px-2 sm:mr-0 sm:mt-12 md:mt-20">
+							<div
+								aria-hidden
+								className="bg-gradient-to-b to-background absolute inset-0 z-10 from-transparent from-35%"
+							/>
+							<div className="inset-shadow-2xs ring-background dark:inset-shadow-white/20 bg-background relative mx-auto max-w-6xl overflow-hidden rounded-2xl border p-4 shadow-lg shadow-zinc-950/15 ring-1">
+								<img
+									className="bg-background aspect-16/8 relative hidden rounded-2xl dark:block"
+									src="/screenshot.png"
+									alt="dark app screen"
+								/>
+								<img
+									className="z-2 aspect-16/8 relative rounded-2xl dark:hidden object-contain"
+									src="/screenshot.png"
+									alt="light app screen"
+								/>
+							</div>
+						</div>
+					</AnimatedGroup>
+				</div>
+			</section>
+			{/* we can move this section to a separate file */}
+			<section className="bg-background pb-16 pt-16 md:pb-32">
+				<div className="group relative m-auto max-w-5xl px-6">
+					<div className="absolute inset-0 z-10 flex scale-95 items-center justify-center opacity-0 duration-500 group-hover:scale-100 group-hover:opacity-100">
+						<Link to="/" className="block text-sm duration-150 hover:opacity-75">
+							<span> Our Customers</span>
+							<ChevronRight className="ml-1 inline-block size-3" />
+						</Link>
+					</div>
+					<div className="group-hover:blur-xs mx-auto mt-12 grid max-w-2xl grid-cols-4 gap-x-12 gap-y-8 transition-all duration-500 group-hover:opacity-50 sm:gap-x-16 sm:gap-y-14">
+						{Array(8)
+							.fill(0)
+							.map((_, index) => (
+								<div className="flex" key={index}>
+									logo {index + 1}
+									{/* <img
 									className="mx-auto h-6 w-fit dark:invert"
 									src="https://html.tailus.io/blocks/customers/openai.svg"
 									alt="OpenAI Logo"
 									height="24"
 									width="auto"
 								/> */}
-									</div>
-								))}
-						</div>
+								</div>
+							))}
 					</div>
-				</section>
-			</main>
-		</>
+				</div>
+			</section>
+		</main>
 	);
 }
--- src/routes/(landing)/index.tsx
+++ src/routes/(landing)/index.tsx
@@ -1,10 +1,16 @@
 import { HeroSection } from "@/routes/(landing)/-components/hero";
 import { createFileRoute } from "@tanstack/react-router";
+import Navbar from "./-components/navbar";
 
 export const Route = createFileRoute("/(landing)/")({
 	component: Home
 });
 
 function Home() {
-	return <HeroSection />;
+	return (
+		<>
+			<Navbar />
+			<HeroSection />
+		</>
+	);
 }
