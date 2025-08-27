54fe55ee31ab46aaf1bb6d69b8568766915852b5
amine-tayani <amine.tayanipro@gmail.com>
2025-08-27 22:55:16 +0100

    feat: move expenses card to transactions page

--- src/routes/dashboard/index.tsx
+++ src/routes/dashboard/index.tsx
@@ -1,4 +1,3 @@
-import { InsightCard } from "@/components/dashboard/insight-card";
 import { InteractiveChart } from "@/components/interactive-chart";
 import { createFileRoute } from "@tanstack/react-router";
 import type { User } from "better-auth";
@@ -10,43 +9,10 @@ export const Route = createFileRoute("/dashboard/")({
 	}
 });
 
-const data = [
-	{
-		title: "Total Balance",
-		value: "$1234.56",
-		changePercent: "-4.6%"
-	},
-	{
-		title: "Monthly Income",
-		value: "$583.54",
-		changePercent: "-4%"
-	},
-	{
-		title: "Monthly Expenses",
-		value: "$1,000",
-		changePercent: "+10%"
-	},
-	{
-		title: "Savings",
-		value: "$800.32",
-		changePercent: "-2.5%"
-	}
-];
-
 function DashboardPage() {
 	return (
 		<div className="flex flex-1 flex-col gap-4 lg:gap-6 mt-4 mx-7 mb-4">
-			<div className="grid grid-cols-2 min-[1200px]:grid-cols-4 gap-4">
-				{data.map((item) => (
-					<InsightCard
-						key={item.title}
-						title={item.title}
-						value={item.value}
-						changePercent={item.changePercent}
-					/>
-				))}
-			</div>
-			<div className="min-h-[100vh] flex-1 md:min-h-min">
+			<div className="h-screen min-h-[570px] flex-1">
 				<InteractiveChart />
 			</div>
 		</div>
--- src/routes/dashboard/transactions.tsx
+++ src/routes/dashboard/transactions.tsx
@@ -1,3 +1,4 @@
+import { InsightCard } from "@/components/dashboard/insight-card";
 import { TransactionsTable } from "@/components/dashboard/transactions-table";
 import { db } from "@/lib/db";
 import { transactions } from "@/lib/db/schema";
@@ -23,11 +24,42 @@ export const Route = createFileRoute("/dashboard/transactions")({
 
 function TransactionsPage() {
 	const { transactions } = Route.useLoaderData();
-
+	const data = [
+		{
+			title: "Total Balance",
+			value: "$1234.56",
+			changePercent: "-4.6%"
+		},
+		{
+			title: "Monthly Income",
+			value: "$583.54",
+			changePercent: "-4%"
+		},
+		{
+			title: "Monthly Expenses",
+			value: "$1,000",
+			changePercent: "+10%"
+		},
+		{
+			title: "Savings",
+			value: "$800.32",
+			changePercent: "-2.5%"
+		}
+	];
 	return (
 		<div className="flex h-full">
 			<main className="flex-1 flex flex-col h-screen overflow-hidden">
 				<div className="flex-1 overflow-y-auto flex flex-col gap-4 mx-7 mb-4">
+					<div className="grid grid-cols-2 min-[1200px]:grid-cols-4 gap-4 mt-2 mb-4">
+						{data.map((item) => (
+							<InsightCard
+								key={item.title}
+								title={item.title}
+								value={item.value}
+								changePercent={item.changePercent}
+							/>
+						))}
+					</div>
 					<TransactionsTable data={transactions} />
 				</div>
 			</main>
