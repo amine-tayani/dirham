a04536711b6d22b416e40250a6ff2b6ce94da473
amine-tayani <amine.tayanipro@gmail.com>
2025-09-02 18:18:11 +0100

    Add new transactions stats cards

--- src/routes/dashboard/transactions.tsx
+++ src/routes/dashboard/transactions.tsx
@@ -1,5 +1,5 @@
-import { InsightCard } from "@/components/dashboard/insight-card";
 import { TransactionsTable } from "@/components/dashboard/transactions-table";
+import { Card, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { db } from "@/lib/db";
 import { transactions } from "@/lib/db/schema";
 import { createFileRoute } from "@tanstack/react-router";
@@ -27,31 +27,38 @@ function TransactionsPage() {
 	const data = [
 		{
 			title: "Total Transactions",
-			value: "5"
+			value: 4
 		},
 		{
 			title: "Income",
-			value: "$1583.54",
-			changePercent: "+3.5%"
+			value: 583.54
 		},
 		{
 			title: "Expenses",
-			value: "$683.54",
-			changePercent: "+7.5%"
+			value: 683.54
 		}
 	];
 	return (
 		<div className="flex h-full">
 			<main className="flex-1 flex flex-col h-screen overflow-hidden">
 				<div className="flex-1 overflow-y-auto flex flex-col gap-4 mx-7 mb-4">
-					<div className="grid grid-cols-2 min-[1200px]:grid-cols-4 gap-4 mt-2 mb-4">
+					<div className="grid grid-cols-1 min-[1200px]:grid-cols-5 gap-4 mt-2 mb-4">
 						{data.map((item) => (
-							<InsightCard
-								key={item.title}
-								title={item.title}
-								value={item.value}
-								changePercent={item.changePercent}
-							/>
+							<Card className="flex flex-col border-border shadow-none" key={item.title}>
+								<CardHeader className="relative">
+									<CardDescription className="text-sm font-medium -mt-2">
+										<span>{item.title}</span>
+									</CardDescription>
+									<CardTitle className="@[250px]/card:text-3xl text-2xl font-semibold tabular-nums mt-2">
+										{Number.isInteger(item.value)
+											? item.value
+											: new Intl.NumberFormat("en-US", {
+													style: "currency",
+													currency: "USD"
+												}).format(item.value)}
+									</CardTitle>
+								</CardHeader>
+							</Card>
 						))}
 					</div>
 					<TransactionsTable data={transactions} />
