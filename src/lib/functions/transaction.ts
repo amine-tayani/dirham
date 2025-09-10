9646fb56a3b6dbaba960064b9fb44f0c97b255f0
amine-tayani <amine.tayanipro@gmail.com>
2025-09-10 21:10:43 +0100

    feat: Implement  AuthMiddleware

--- src/lib/functions/transaction.ts
+++ src/lib/functions/transaction.ts
@@ -1,9 +1,10 @@
 import { transactionFormSchema } from "@/lib/db/schema";
 import { createServerFn } from "@tanstack/react-start";
 
+// we need to add an auth middleware here since we are adding a transaction
 export const createTransactionFn = createServerFn({ method: "POST" })
 	.validator(transactionFormSchema.parse)
-	.handler(async ({ data }) => {
-		console.log(data);
+	.handler(async ({ data, context }) => {
+		console.log(context);
 		return data;
 	});
---
+++ src/utils/authMiddleware.ts
@@ -0,0 +1,23 @@
+import { auth } from "@/lib/server/auth";
+import { redirect } from "@tanstack/react-router";
+import { createMiddleware } from "@tanstack/react-start";
+import { getWebRequest } from "@tanstack/react-start/server";
+
+export const AuthMiddleware = createMiddleware({
+	type: "function"
+}).server(async ({ next }) => {
+	const request = getWebRequest();
+
+	if (!request?.headers) {
+		throw redirect({ to: "/" });
+	}
+	const session = await auth.api.getSession({ headers: request.headers });
+
+	if (!session) {
+		throw redirect({ to: "/" });
+	}
+
+	return next({
+		context: { userId: session.user.id }
+	});
+});
