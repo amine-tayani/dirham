ee75d62bd6311822a3d77449fb986c5e3c3874e0
amine-tayani <amine.tayanipro@gmail.com>
2025-09-11 18:40:48 +0100

    feat: Add authentication middleware to transaction

--- src/lib/functions/transaction.ts
+++ src/lib/functions/transaction.ts
@@ -1,9 +1,13 @@
 import { transactionFormSchema } from "@/lib/db/schema";
+import { AuthMiddleware } from "@/utils/authMiddleware";
 import { createServerFn } from "@tanstack/react-start";
 
-// we need to add an auth middleware here since we are adding a transaction
+// We need to use drizzle inside our handler to persist the data
+// we need to use tanstack query more often
+
 export const createTransactionFn = createServerFn({ method: "POST" })
 	.validator(transactionFormSchema.parse)
+	.middleware([AuthMiddleware])
 	.handler(async ({ data, context }) => {
 		console.log(context);
 		return data;
